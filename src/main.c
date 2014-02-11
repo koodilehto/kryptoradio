deb0c9bdd4eeadc3e47f11552cdea7257c0c4db0
Joel Lehtonen <joel.lehtonen@koodilehto.fi>
2014-02-11 03:01:48 +0200

    Reads packets

--- src/main.c
+++ src/main.c
@@ -13,6 +13,7 @@
 //#include <errno.h>
 #include <arpa/inet.h> 
 #include <err.h>
+#include <stdbool.h>
 
 uint8_t join_message[] = {
 	0xF9, 0xBE, 0xB4, 0xD9, 0x76, 0x65, 0x72, 0x73, 0x69, 0x6F,
@@ -28,6 +29,13 @@ uint8_t join_message[] = {
 	0x8C, 0xE6, 0x17, 0x65, 0x0F, 0x2F, 0x53, 0x61, 0x74, 0x6F,
 	0x73, 0x68, 0x69, 0x3A, 0x30, 0x2E, 0x37, 0x2E, 0x32, 0x2F,
 	0xC0, 0x3E, 0x03, 0x00
+};
+
+struct msg_header {
+	uint32_t magic;
+	char command[12];
+	uint32_t length;
+	uint32_t checksum;
 };
 
 const int join_message_len = sizeof(join_message);
@@ -59,18 +67,39 @@ int main(int argc, char *argv[])
 		err(2,"Connect Failed");
 	} 
 
+	// Making FILE from socket fd
+	FILE *bitcoind = fdopen(sockfd,"r+");
+	if (bitcoind == NULL) err(2,"Buffered reader has failed");
+
 	// Send greeting
-	if (write(sockfd,join_message,join_message_len) < 0) {
-		err(2,"Sending of welcome message has failed");
-	}		
+	if (fwrite(join_message,join_message_len,1,bitcoind) != 1) {
+		errx(2,"Sending of welcome message has failed");
+	}
 
 	printf("Connected.\n");
 
-	while ( (n = read(sockfd, recvBuff, sizeof(recvBuff)-1)) > 0)
-	{
-		printf("saatiin %d\n",n);
-	} 
+	// Message receiver loop. Processes messages having this format:
+	// https://en.bitcoin.it/wiki/Protocol_specification#Message_structure
+	while (true) {
+		// Take header
+		struct msg_header header;
 
-	// If we got out of while loop then we have failed
-	err(3,"Connection dead");
+		if (fread(&header,sizeof(header),1,bitcoind) != 1) {
+			errx(3,"Receiving data from bitcoind has failed");
+		}
+
+		// FIXME One should change byte ordering on big-endian machines!
+
+		printf("%s, %d tavua.\n",header.command,header.length);
+
+		if (header.length > sizeof(recvBuff)) {
+			errx(4,"Too small receive buffer, exiting.");
+		}
+
+		if (header.length != 0 && fread(recvBuff,header.length,1,bitcoind) != 1) {
+			errx(3,"Receiving data from bitcoind has failed");
+		}
+	}
+
+	// Never reached
 }
