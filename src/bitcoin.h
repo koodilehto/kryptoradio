4fa34d976b7b34c2476f0cfe8aa5526ad7fe21b7
Joel Lehtonen <joel.lehtonen@koodilehto.fi>
2014-02-16 17:39:23 +0200

    Move bitcoin_join() to bitcoin.c

--- src/bitcoin.h
+++ src/bitcoin.h
@@ -5,6 +5,9 @@
 #ifndef BITCOIN_H_
 #define BITCOIN_H_
 
+#include <glib.h>
+#include <stdbool.h>
+
 struct __attribute__ ((__packed__)) msg {
 	guint32 magic;
 	char command[12];
@@ -20,5 +23,6 @@ gint var_int_len(const guint8 *const buf);
 guint32 checksum(const struct msg *const m);
 unsigned char *dhash(const guchar *const d, const gulong n);
 char *hex256(const guchar *const buf);
+bool bitcoin_join(int fd);
 
 #endif /* BITCOIN_H_ */
