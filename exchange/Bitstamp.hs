1b147248a9c42862b0ef4a67af1ab683a8861fd9
Joel Lehtonen <joel.lehtonen@koodilehto.fi>
2014-08-25 01:00:57 +0300

    Bitstamp parser is able to get bids, asks and trades

--- exchange/Bitstamp.hs
+++ exchange/Bitstamp.hs
@@ -1,14 +1,38 @@
-{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE OverloadedStrings, RecordWildCards #-}
 -- |Connects and parses Bitstamp exchange feed
 module Bitstamp (bitstamp) where
 
+import Control.Applicative
+import Control.Monad
 import Control.Concurrent.STM.TChan
 import Data.Text (Text)
+import Data.Aeson
+import Data.Aeson.Types
 import Pusher
 
--- |Subscriptions to Pusher channels
+data Entry = Bid | Ask | Trade deriving (Show)
+
+-- |Subscriptions to Bitstamp live order book and trade stream
 subs :: [Text]
 subs = ["diff_order_book","live_trades"]
 
-bitstamp :: IO (TChan Pusher)
-bitstamp = connectPusher "ws.pusherapp.com" 80 "de504dc5763aeef9ff52" subs Just
+extract Pusher{..} = case (event,channel) of
+  ("data","diff_order_book") -> either fail Just $ parseEither orderParser payload
+  ("trade","live_trades") -> either fail Just $ parseEither tradeParser payload
+  _ -> Nothing
+  where
+    -- Dig all bids and asks and capsule them again
+    orderParser (Object o) = do
+      bids <- map (conv Bid) <$> o .: "bids"
+      asks <- map (conv Ask) <$> o .: "asks"
+      return $ bids++asks
+    orderParser _ = mzero
+    -- Trades happen once at a time. Take it out and put inside singleton list
+    tradeParser (Object o) = do
+      price <- o .: "price"
+      amount <- o .: "amount"
+      return [((Trade,price),amount)]
+    conv entry (pos,amount) = ((entry,read pos),read amount)
+
+bitstamp :: IO (TChan [((Entry,Double),Double)])
+bitstamp = connectPusher "ws.pusherapp.com" 80 "de504dc5763aeef9ff52" subs extract
