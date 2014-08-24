f15ef47760721cc9b14df43b4e4d2d0ddcc2a91c
Joel Lehtonen <joel.lehtonen@koodilehto.fi>
2014-08-24 23:09:54 +0300

    Move Pusher related parts to its own module

---
+++ exchange/Pusher.hs
@@ -0,0 +1,49 @@
+{-# LANGUAGE OverloadedStrings #-}
+-- |Handles Websocket connection to Pusher and parses its messages
+module Pusher (Pusher(..),connectPusher) where
+
+import Control.Applicative
+import Control.Concurrent (forkFinally)
+import Control.Concurrent.STM.TChan
+import Control.Exception.Base (SomeException,throw)
+import Control.Monad (forever,mzero)
+import Control.Monad.STM
+import Data.Aeson
+import Data.ByteString (ByteString)
+import Data.Text (Text)
+import Network.WebSockets (ClientApp,Connection,sendTextData,runClient,receiveData)
+
+data Pusher = Pusher { event   :: Text
+                     , payload :: Value
+                     , channel :: Text
+                     } deriving (Show)
+
+instance FromJSON Pusher where
+  parseJSON (Object v) = Pusher <$>
+                         v .: "event" <*>
+                         (v .: "data" >>= either fail return . eitherDecode') <*>
+                         v .:? "channel" .!= ""
+  parseJSON _ = mzero
+
+app :: TChan Pusher -> [ByteString] -> ClientApp ()
+app chan subs conn = do
+  -- Subscribe to Pusher channels
+  mapM_ (sendTextData conn) subs
+  -- Do actions
+  forever $ do
+    msg <- receiveData conn
+    atomically $ writeTChan chan $ case eitherDecode' msg of
+      Left e -> error e
+      Right a -> a
+
+-- |Pass the grenade to the channel
+rethrow :: TChan a -> Either SomeException b -> IO ()
+rethrow ch x = atomically $ writeTChan ch $ case x of
+  Right _ -> error "Web socket closed"
+  Left e  -> throw e
+
+connectPusher :: String -> Int -> String -> [ByteString] -> IO (TChan Pusher)
+connectPusher host port appKey subs = do
+  chan <- newTChanIO
+  forkFinally (runClient host port ("/app/" ++ appKey ++ "?protocol=7") $ app chan subs) (rethrow chan)
+  return chan
