7ab1d23985d63c2ace5ffd19cfdd1a00b38dbd80
Joel Lehtonen <joel.lehtonen@koodilehto.fi>
2014-08-25 02:46:10 +0300

    Support for dumping differences in order book

--- exchange/Book.hs
+++ exchange/Book.hs
@@ -16,10 +16,12 @@ main = do
   ch <- bitstamp
   book <- newTVarIO M.empty
   forkIO $ orderbook ch book
-  forever $ do
-    getLine
-    stuff <- readTVarIO book
-    print stuff
+  loop book M.empty
+  where loop book old = do
+          getLine
+          new <- readTVarIO book
+          print $ bookDiff old new
+          loop book new
 
 orderbook :: TChan [Entry] -> TVar (Map Key Double) -> IO ()
 orderbook ch book = forever $ atomically $ do
@@ -33,3 +35,17 @@ orderbook ch book = forever $ atomically $ do
           (Bid,_)   -> M.insert key value
           (Ask,_)   -> M.insert key value
 
+bookDiff :: Map Key Double -> Map Key Double -> Map Key Double
+bookDiff = M.mergeWithKey common onlyInOld onlyInNew
+  where
+    -- If a value is in both old and new
+    common Key{..} a b = case (kind,a==b) of
+      (_,True)  -> Nothing    -- No changes, not interesting
+      (Trade,_) -> Just (b-a) -- In cases of trades, send change
+      (_,_)     -> Just b     -- In cases of bid and ask, use newer value
+    -- If a value is absent from new, it is a sign that bid/ask is
+    -- removed. Then we give a value of zero. Trades are never removed
+    -- so they are not checked.
+    onlyInOld = M.map (const 0)
+    -- Keep new trades and orders if present only in new
+    onlyInNew = id
