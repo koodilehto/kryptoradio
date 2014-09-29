7c75d5734de156c96f3b7ab8c9326004699fd8b4
Joel Lehtonen <joel.lehtonen@koodilehto.fi>
2014-09-29 14:52:20 +0300

    Add command-line parser for receiver

--- haskell/Kryptoradio/Receiver/Main.hs
+++ haskell/Kryptoradio/Receiver/Main.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE DeriveDataTypeable, RecordWildCards #-}
 module Main where
 
 import Control.Monad
@@ -6,14 +6,50 @@ import qualified Data.ByteString.Lazy.Char8 as B
 import Control.Concurrent (forkIO)
 import Control.Concurrent.STM
 import Control.Concurrent.STM.TChan (readTChan,newTChanIO)
+import Data.String (fromString)
+import Network.Wai
+import Network.Wai.Handler.Warp
+import System.Console.CmdArgs.Implicit hiding (name)
 import System.IO (stdout) -- temp
 import Kryptoradio.Receiver.Dvb
 import Kryptoradio.Receiver.Parser
 import Kryptoradio.Receiver.Resources
 
+data Args = Args { device   :: Int
+                 , frontend :: Int
+                 , demuxer  :: Int
+                 , freq     :: Int
+                 , pid      :: Int
+                 , host     :: String
+                 , port     :: Int
+                 } deriving (Show, Data, Typeable)
+
+synopsis = Args { device = 0 &= help "DVB device id (default: 0)"
+                , frontend = 0 &= help "DVB frontend id (default: 0)"
+                , demuxer = 0 &= help "DVB demuxer id (default: 0)"
+                , freq = 0 &= argPos 0 &= typ "HERTZ"
+                , pid = 8101 &= help "Kryptoradio PID (default: 8101)"
+                , host = "*" &= help "IP address to bind to (default: all)"
+                , port = 3000 &= help "HTTP port to listen to (default: 3000)"
+                }
+           &= program "kryptoradio-receiver"
+           &= summary "Kryptoradio Receiver v0.0.1"
+           &= help "Listens to given HTTP port for connections while \
+                   \receiving and decoding data from DVB device connected \
+                   \to the system. If you have only one ordinary DVB-adapter \
+                   \in your system, you don't need to set device, frontend \
+                   \nor demuxer IDs. If you are receiving from Digita \
+                   \broadcast in Finland, the default PID is also fine. \
+                   \Frequency must be given in Hz."
+
 main = do
-  putStrLn "It compiles! Hard coded tuning to 634 MHz, listening to PID 8101"
-  (h,dvb) <- openDvb 0 0 0 634000000 8101
+  Args{..} <- cmdArgs synopsis
+  let set = setHost (fromString host) $
+            setPort port $
+            defaultSettings
+  putStrLn $ "Tuning to " ++ show (fromIntegral freq / 1e6) ++ "MHz, PID " ++ show pid
+  putStrLn $ "Binding to " ++ show (getHost set) ++ ", port " ++ show (getPort set)
+  (h,dvb) <- openDvb device frontend demuxer freq pid
   resVar <- newTVarIO []
   -- FIXME if using threaded runtime the handle has extremely high
   -- latency (minutes) when run inside forkIO
